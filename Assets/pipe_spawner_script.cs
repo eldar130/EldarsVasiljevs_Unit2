4fd8fccaa4f0e23ab73720261aa70859b6371844
eldar130 <veldarv@gmail.com>
2025-12-02 20:59:04 +0000

    all menu UI added
    title, button and game over text have been added

--- Assets/pipe_spawner_script.cs
+++ Assets/pipe_spawner_script.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class pipe_spawner_script : MonoBehaviour
 {
     [SerializeField] private float spawnTime = 2.5f;
-    [SerializeField] private float heightTop = 1.1f;
-    [SerializeField] private float heightBottom = -0.7f;
+    [SerializeField] private float rangeTop = 1.1f;
+    [SerializeField] private float rangeBottom = -0.7f;
     [SerializeField] private GameObject pipe;
     private float timer;
 
@@ -25,7 +25,7 @@ public class pipe_spawner_script : MonoBehaviour
 
     void spawner()
     {
-        Vector3 spawnPos = transform.position + new Vector3(0, Random.Range(heightBottom, heightTop));
+        Vector3 spawnPos = transform.position + new Vector3(0, Random.Range(rangeBottom, rangeTop));
         GameObject pipes = Instantiate(pipe, spawnPos, Quaternion.identity);
 
         Destroy(pipes, 8f);
