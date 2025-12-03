7337dfabba20b7d8b2a59e3859fe4c75a89054d1
eldar130 <veldarv@gmail.com>
2025-12-03 20:54:38 +0000

    infinite floor fixed
    floor is the same speed as pipes and there is no lag when reset to start the tiling from start position

--- Assets/loop_floor_script.cs
+++ Assets/loop_floor_script.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class loop_floor_script : MonoBehaviour
 {
-    [SerializeField] private float speed = 1.3f;
-    [SerializeField] private float width = 0.2f;
-    private SpriteRenderer spriteRenderer;
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float width = 35.2f;
+    private SpriteRenderer _spriteRenderer;
     private Vector2 startSize;
 
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        startSize = new Vector2(spriteRenderer.size.x, spriteRenderer.size.y);
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        startSize = new Vector2(_spriteRenderer.size.x, _spriteRenderer.size.y);
     }
 
     void Update()
     {
-        spriteRenderer.size = new Vector2(spriteRenderer.size.x + speed * Time.deltaTime, spriteRenderer.size.y);
-        if (spriteRenderer.size.x > width)
+        _spriteRenderer.size = new Vector2(_spriteRenderer.size.x + speed * Time.deltaTime, _spriteRenderer.size.y);
+        if (_spriteRenderer.size.x > width)
         {
-            spriteRenderer.size = startSize;
+            _spriteRenderer.size = startSize;
         }
     }
 }
